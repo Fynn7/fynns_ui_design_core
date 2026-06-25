a177cd9cc39abea38cc6c2321bebb81c88ba4e50
Fynn7 <fynnandreas7@gmail.com>
2026-06-25 19:54:25 +0200

    feat(icons): add PanelLeftIcon for sidebar toggles

--- src/primitives/icons.tsx
+++ src/primitives/icons.tsx
@@ -363,3 +363,12 @@ export function StopIcon(props: IconProps) {
     </svg>
   );
 }
+
+export function PanelLeftIcon(props: IconProps) {
+  return (
+    <svg {...svgProps(props)}>
+      <rect width="18" height="18" x="3" y="3" rx="2" />
+      <path d="M9 3v18" />
+    </svg>
+  );
+}
